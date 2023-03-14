3f0226d2bf9ff65e4707e55d8376412c29ffa952
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-03-14 16:51:39 -0300

    :sparkles: Adicionando as funcionaldiades de persistência local dos dados na memória do dispositivo com Async Storage, aqui temos funções de cadastro, lsitagem remoção e remoção de todos os itens :sparkles:

---
+++ src/storage/skills/deleteSkill.ts
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { MY_SKILLS_COLLECTION } from '../storageConfig';
+import { getAllSkills } from './getAllSkills';
+
+export async function deleteSkill(skillId: string) {
+  try {
+    const skillsStoraged = await getAllSkills();
+
+    const skillsFiltered = skillsStoraged.filter((skill) => skill.id !== skillId);
+
+    const skillsFilteredFormatted = JSON.stringify(skillsFiltered);
+
+    await AsyncStorage.setItem(MY_SKILLS_COLLECTION, skillsFilteredFormatted);
+  } catch (error) {
+    throw error;
+  }
+}
