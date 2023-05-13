ddc0e7391b85a73c2288fcbff89ee9913b1ef860
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-05-13 17:06:00 -0300

    :zap: Melhorando a performance ao definir o Card da listagem antes da renderização e memorizá-lo com useCallback de forma que evite sua recriação desnecessariamente :zap:

--- src/components/SkillCards/index.tsx
+++ src/components/SkillCards/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { FlatList, RefreshControl, StyleSheet, Text, View } from 'react-native';
-import { SkillCard } from '../SkillCard';
-import { ITEM_HEIGHT, SkillItemProps } from '../../dto/skillDTO';
-import theme from '../../theme';
+import React, { useCallback } from 'react';
 
+import { FlatList, RefreshControl, StyleSheet, Text, View } from 'react-native';
+
+import { ITEM_HEIGHT, SkillItemProps } from '../../dto/skillDTO';
+
+import { SkillCard } from '../SkillCard';
+import theme from '../../theme';
 
 interface MySkillsProps {
   mySkillsValues: SkillItemProps[];
@@ -13,15 +15,20 @@ interface MySkillsProps {
 }
 
 const keyExtractor = (item: SkillItemProps) => item.id;
-const ItemSeparator = () => <View style={{ height: 16 }} />;
-const ListEmptyComponent = () => <View>
-  <Text style={styles.emptyListText}>Nenhuma habilidade cadastrada</Text>
-</View>;
-const getItemLayout = (_: SkillItemProps[] | null | undefined, index: number) => ({
+const ItemSeparator = () => <View style={styles.itemSeparator} />;
+const ListEmptyComponent = () => (
+  <View>
+    <Text style={styles.emptyListText}>Nenhuma habilidade cadastrada</Text>
+  </View>
+);
+const getItemLayout = (
+  _: SkillItemProps[] | null | undefined,
+  index: number,
+) => ({
   length: ITEM_HEIGHT,
   offset: ITEM_HEIGHT * index,
-  index
-})
+  index,
+});
 
 export function SkillCards({
   isLoading,
@@ -29,32 +36,40 @@ export function SkillCards({
   RemoveSkill,
   mySkillsValues,
 }: MySkillsProps) {
-  const SkillCardItemList = ({ item }: { item: SkillItemProps }) => (
-    <SkillCard item={item} RemoveSkill={RemoveSkill} />
+  const renderItem = useCallback(
+    ({ item }: { item: SkillItemProps }) => (
+      <SkillCard item={item} RemoveSkill={RemoveSkill} />
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
   );
-
   return (
-    <FlatList
-      data={mySkillsValues}
-      keyExtractor={keyExtractor}
-      getItemLayout={getItemLayout}
-      renderItem={SkillCardItemList}
-      ItemSeparatorComponent={ItemSeparator}
-      ListEmptyComponent={ListEmptyComponent}
-      contentContainerStyle={styles.contentContainerStyle}
-      refreshControl={
-        <RefreshControl
-          colors={[theme.COLORS.HIGHLIGHT]}
-          refreshing={isLoading}
-          onRefresh={onRefresh}
-        />
-      }
-    />
-  )
+    <View onStartShouldSetResponder={() => true} style={styles.container}>
+      <FlatList
+        data={mySkillsValues}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={keyExtractor}
+        getItemLayout={getItemLayout}
+        renderItem={renderItem}
+        ItemSeparatorComponent={ItemSeparator}
+        ListEmptyComponent={ListEmptyComponent}
+        contentContainerStyle={styles.contentContainerStyle}
+        refreshControl={
+          <RefreshControl
+            colors={[theme.COLORS.HIGHLIGHT]}
+            refreshing={isLoading}
+            onRefresh={onRefresh}
+          />
+        }
+      />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   emptyListText: {
     color: theme.COLORS.TEXT_PRIMARY,
     fontSize: 16,
@@ -63,6 +78,8 @@ const styles = StyleSheet.create({
   },
   contentContainerStyle: {
     paddingBottom: 10,
-  }
-
-})
+  },
+  itemSeparator: {
+    height: 16,
+  },
+});
