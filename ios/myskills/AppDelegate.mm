ced70b9773dc135d02aa712ebdbf4272b4922516
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-07-11 01:47:50 -0300

    :sparkles: Criação da Splash screen e Configuração para que Splash Screen permaneça em tela enquanto o app carrega :sparkles:

--- ios/myskills/AppDelegate.mm
+++ ios/myskills/AppDelegate.mm
@@ -2,16 +2,25 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import "RNSplashScreen.h" 
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+
   self.moduleName = @"mySkills";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  bool didFinish = [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  [RNSplashScreen show];// here <<<<<<<<<<
+  // or
+  //[RNSplashScreen showSplash:@"LaunchScreen" inRootView:rootView];
+  return didFinish;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
